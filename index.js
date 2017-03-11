fbd57c6c504f7da5ba1fc0876380f781ef661e31
batanda <bkayondo@rwanda.cmu.edu>
2017-03-11 14:32:36 +0200

    brought in index.js.v3

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
 
+// import the functions
+
+var functs = require('./functs');
+
 app.set('port', (process.env.PORT || 5000))
 
 // Process application/x-www-form-urlencoded
@@ -48,6 +52,51 @@ app.get('/webhook', function (req, res)
     }
 });
 
+// Process Facebook Messenger Requests
+
+app.post('/webhook', function (req, res)
+{
+    var data = req.body;
+
+    // Make sure this is a page subscription
+    if (data.object === 'page')
+    {
+        // Iterate over each entry - there may be multiple if batched
+        data.entry.forEach(function(entry)
+        {
+        var pageID = entry.id;
+        var timestamp = entry.time;
+
+        // Iterate over each messaging event
+        entry.messaging.forEach(function(event)
+        {
+            if (event.message)
+            {
+                if (event.message.is_echo)
+                      console.log("Bot received message written event");
+                else
+                      console.log("Bot received message " + event.message.text);
+            }
+            else  if (event.delivery)
+              console.log("Bot received delivery event");
+            else  if (event.read)
+              console.log("Bot received message-was-read event");
+            else  if (event.postback)
+              functs.doPostback(event);
+            else
+              console.log("Bot received unknown EVENT: ");
+          });
+        });
+    }
+    else
+    {
+      console.log("Bot received unknown OBJECT (not page): ", data.object);
+    }
+
+    // All good, sent response status 200
+
+    res.sendStatus(200)
+});
 
 // Spin up the server
 app.listen(app.get('port'), function()
