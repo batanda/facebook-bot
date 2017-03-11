df66c38389167e5312a4890594348269f4f07636
batanda <bkayondo@rwanda.cmu.edu>
2017-03-11 18:51:26 +0200

    edited the doPostback function, under the functs.js

--- functs.js
+++ functs.js
@@ -123,7 +123,22 @@ sendGenericMessage: function(toId)
 
 doPostback: function(event)
 {
-  console.log("Stub: process the postback");
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfPostback = event.timestamp;
+
+  // The 'payload' param is a developer-defined field which is set in a postback
+  // button for Structured Messages.
+  var payload = event.postback.payload;
+  var myResponse = payload.replace("me", "you");
+
+  console.log("Received postback for user %d and page %d with payload '%s' " +
+    "at %d", senderID, recipientID, payload, timeOfPostback);
+
+  // When a postback is called, we'll send a message back to the sender to
+  // let them know it was successful
+
+  this.sendTextMessage(senderID, "I will gladly " + myResponse);
 },
 
 };
