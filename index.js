defd95c3d4d55a97a31c3ae84867288c238c5637
batanda <bkayondo@rwanda.cmu.edu>
2017-03-11 14:25:02 +0200

    add webhook token

--- index.js
+++ index.js
@@ -19,6 +19,35 @@ app.get('/', function (req, res)
     res.send('Hello World, I would like to be a chat bot')
 })
 
+// for Facebook verification
+app.get('/webhook', function (req, res)
+{
+    console.log("/webhook Verify Token");
+
+    // Facebook verification
+
+    console.log("Verify Token sent");
+    if (req.query['hub.mode'] === 'subscribe')
+    {
+        console.log("Subscribe Verify Token, check: " + req.query['hub.verify_token']);
+        if (req.query['hub.verify_token'] === 'just_net_token')
+        {
+            console.log("Validating webhook");
+            res.send(req.query['hub.challenge'])
+        }
+        else
+        {
+            console.log("Oh No, invalid Verify Token: " + req.query['hub.verify_token']);
+            res.send("Error, wrong token.");
+        }
+    }
+    else
+    {
+        console.log("not subscribing");
+        res.send('Hello world')
+    }
+});
+
 
 // Spin up the server
 app.listen(app.get('port'), function()
